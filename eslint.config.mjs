cd92c173b632961afc20c9f5d9af7a67ca2b0fd2
Salman butt <salman0butt@gmail.comsource>
2025-11-27 00:20:59 +0500

    eslint config updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,22 +5,22 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig(
-    {
-        ignores: ['dist/**', 'node_modules/**', 'eslint.config.mjs'],
+  {
+    ignores: ['dist/**', 'node_modules/**', 'eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    {
-        files: ['**/*.ts'],
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
-            },
-        },
-        rules: {
-            // 'no-console': 'error',
-            // 'dot-notation': 'error',
-        },
+    rules: {
+      // 'no-console': 'error',
+      // 'dot-notation': 'error',
     },
+  },
 );

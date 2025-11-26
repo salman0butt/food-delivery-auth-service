0222612b91c216c0a75c22a649a7c8e65da6083c
Salman butt <salman0butt@gmail.comsource>
2025-11-27 00:17:32 +0500

    test lint staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,7 @@ export default defineConfig(
         },
         rules: {
             // 'no-console': 'off',
+            'dot-notation': 'error',
         },
     }
 );
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 function welcome() {
-    console.log('Welcome to the Auth Service');
+  console.log('Welcome to the Auth Service');
+
+  const user = {
+    name: 'Salman Butt',
+  };
+
+  const fname = user.name;
+
+  return name + fname;
 }
 
 welcome();
